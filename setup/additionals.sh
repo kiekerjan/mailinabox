2474ff36494b9e305cd8f6ed86cb3d93bf38690d
KiekerJan <github@kiekerjan.isdronken.nl>
2021-02-19 22:20:52 +0100

    cleaner way of cleaning old mail, force enable spamassassin

--- setup/additionals.sh
+++ setup/additionals.sh
@@ -3,8 +3,7 @@ source setup/functions.sh
 
 # Cleanup old spam and trash email
 cp -f conf/local_clean_mail /etc/cron.weekly/
-chmod +x /etc/cron.weekly
-sed -i "s/PRIMARY_HOSTNAME/$PRIMARY_HOSTNAME/g" /etc/cron.weekly/local_clean_mail
+chmod +x /etc/cron.weekly/local_clean_mail
 
 # rootkit hunter
 apt_install rkhunter
