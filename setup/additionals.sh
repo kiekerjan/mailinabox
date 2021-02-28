b3b39fe0157672546c6b29752e5c58ebd5c9b42a
KiekerJan <github@kiekerjan.isdronken.nl>
2021-02-28 22:26:00 +0100

    cron work

--- setup/additionals.sh
+++ setup/additionals.sh
@@ -2,8 +2,12 @@ source /etc/mailinabox.conf
 source setup/functions.sh
 
 # Cleanup old spam and trash email
-cp -f conf/local_clean_mail /etc/cron.weekly/
+cp -f conf/cron/local_clean_mail /etc/cron.weekly/
 chmod +x /etc/cron.weekly/local_clean_mail
+
+# Explicitly reopen dovecot logfiles after logrotate has run
+cp -f conf/cron/logrotate_post_dovecot /etc/cron.daily/
+chmod +x /etc/cron.daily/logrotate_post_dovecot
 
 # Reduce logs by not logging mail output in syslog
 sed -i "s/\*\.\*;auth,authpriv.none.*\-\/var\/log\/syslog/\*\.\*;mail,auth,authpriv.none    \-\/var\/log\/syslog/g" /etc/rsyslog.d/50-default.conf
