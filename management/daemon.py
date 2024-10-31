f205c7df5818a87abc9a5862fe185957c0085db9
KiekerJan <kiekerjan@users.noreply.github.com>
2024-10-31 16:32:32 +0100

    fixes to the advanced DNS panel

--- management/daemon.py
+++ management/daemon.py
@@ -407,7 +407,7 @@ def dns_get_zonefile(zone):
 @authorized_personnel_only
 def dns_get_advanced_dns_options():
 	
-	return json_response({ "hiddenmaster_enabled": False, "hiddenmaster_selected": False, "short_ttl_selected": False })
+	return json_response({ "hiddenmaster_enabled": False, "hiddenmaster_selected": True, "short_ttl_selected": True	})
 
 @app.route('/dns/advanced-dns', methods=['POST'])
 @authorized_personnel_only
