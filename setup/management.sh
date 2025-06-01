67964e98b891d0b5e42de0168e9a00f15a84b93c
KiekerJan <kiekerjan@users.noreply.github.com>
2025-06-01 09:46:04 +0200

    raise cryptography lib version

--- setup/management.sh
+++ setup/management.sh
@@ -46,7 +46,7 @@ hide_output $venv/bin/pip install --upgrade \
 	rtyaml "email_validator>=1.0.0" "exclusiveprocess" \
 	flask dnspython python-dateutil expiringdict gunicorn \
 	qrcode[pil] pyotp \
-	"idna>=2.0.0" "cryptography==37.0.2" psutil postfix-mta-sts-resolver \
+	"idna>=2.0.0" "cryptography>=45.0.0,<46.0.0" psutil postfix-mta-sts-resolver \
 	b2sdk boto3
 
 # CONFIGURATION
